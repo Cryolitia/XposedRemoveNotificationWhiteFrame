dfd33eae67808975905d6dffb76131f06fe61aec
singleNeuron <liziyuan123456@yeah.net>
2020-05-21 12:23:47 +0200

    try to support miui12

--- app/src/main/java/cn/nexus6p/removewhitenotificationforbugme/main.java
+++ app/src/main/java/cn/nexus6p/removewhitenotificationforbugme/main.java
@@ -17,8 +17,6 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class main implements IXposedHookInitPackageResources, IXposedHookLoadPackage {
 
     private static ArrayList<String> nameStrings = new ArrayList<>();
-    private final static String minHeight = "notification_min_height_legacy";
-    private final static int MINHEIGHT = 133;
     static {
         nameStrings.add("notification_background_custom_padding_left");
         nameStrings.add("notification_background_custom_padding_top");
@@ -28,19 +26,11 @@ public class main implements IXposedHookInitPackageResources, IXposedHookLoadPac
         nameStrings.add("notification_custom_view_margin_start");
         nameStrings.add("notification_custom_view_corner_radius");
         nameStrings.add("notification_row_extra_padding");
+        nameStrings.add("notification_custom_view_margin");
     }
 
     @Override
     public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
-        /*if (!resparam.packageName.equals("com.android.systemui")) return;
-        final int leftID = resparam.res.getIdentifier("notification_background_custom_padding_left","dimen","com.android.systemui");
-        final int topID = resparam.res.getIdentifier("notification_background_custom_padding_top","dimen","com.android.systemui");
-        final int rightID = resparam.res.getIdentifier("notification_background_custom_padding_right","dimen","com.android.systemui");
-        final int bottomID = resparam.res.getIdentifier("notification_background_custom_padding_bottom","dimen","com.android.systemui");
-        resparam.res.setReplacement(leftID,0);
-        resparam.res.setReplacement(topID,0);
-        resparam.res.setReplacement(rightID,0);
-        resparam.res.setReplacement(bottomID,0);*/
         for (String nameString : nameStrings) {
             try {
                 final int ID = resparam.res.getIdentifier(nameString, "dimen", "com.android.systemui");
@@ -60,10 +50,6 @@ public class main implements IXposedHookInitPackageResources, IXposedHookLoadPac
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
                 Context context = (Context) param.args[0];
-                /*final int leftID = context.getResources().getIdentifier("notification_background_custom_padding_left","dimen","com.android.systemui");
-                final int topID = context.getResources().getIdentifier("notification_background_custom_padding_top","dimen","com.android.systemui");
-                final int rightID = context.getResources().getIdentifier("notification_background_custom_padding_right","dimen","com.android.systemui");
-                final int bottomID = context.getResources().getIdentifier("notification_background_custom_padding_bottom","dimen","com.android.systemui");*/
                 final ArrayList<Integer> IDs = new ArrayList<>();
                 for (String nameString : nameStrings) {
                     try {
@@ -75,7 +61,6 @@ public class main implements IXposedHookInitPackageResources, IXposedHookLoadPac
                 XposedHelpers.findAndHookMethod(Resources.class, "getDimensionPixelSize", int.class, new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        super.afterHookedMethod(param);
                         int resID = (int) param.args[0];
                         for (Integer ID : IDs) {
                             if (resID == ID) {
